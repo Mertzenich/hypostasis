f12afb13b2452116dc95bde204323772072cc74d
Adam Mertzenich <adam+git@mertzeni.ch>
2023-11-16 02:02:57 -0600

    Add firewall-add to plugin base protocol

--- src/hypostasis/plugins/base.clj
+++ src/hypostasis/plugins/base.clj
@@ -4,4 +4,5 @@
   "Remote server provider implementation"
   (ip [this] "Access remote server IP")
   (provision [this] "Provision a remote server")
+  (firewall-add [this new])
   (destroy [this] "Destroy a remote server"))
