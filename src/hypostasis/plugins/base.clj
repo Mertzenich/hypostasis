26eb08e018e6d18e01bed9d60a6346b77030addd
Adam Mertzenich <adam+git@mertzeni.ch>
2023-12-05 16:28:39 -0600

    Fix core conflicts

--- src/hypostasis/plugins/base.clj
+++
@@ -1,8 +0,0 @@
-(ns hypostasis.plugins.base)
-
-(defprotocol Plugin
-  "Remote server provider implementation"
-  (ip [this] "Access remote server IP")
-  (provision [this] "Provision a remote server")
-  (firewall-add [this new])
-  (destroy [this] "Destroy a remote server"))
